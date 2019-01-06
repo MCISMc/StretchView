44c277de27c15d91fdf7fc5010895cfcd0c3cb55
Pishang Ujeniya <pishangujeniya@outlook.com>
2019-01-06 19:15:05 +0530

    Improved Performance
    Local Loading of bootstrap css.
    Added js function for getting base64 from image url

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var isOn = -1;
-    chrome.storage.local.get("extensionMode",function (results){
+    chrome.storage.local.get("extensionMode", function (results) {
         var mode = results.extensionMode;
-        switch(mode) {
-                // 0: off; 1: stretch; 2: fix-aspect-ratio;
+        switch (mode) {
+            // 0: off; 1: stretch; 2: fix-aspect-ratio;
             case 0:
                 $("#off").prop("checked", true);
                 break;
@@ -15,31 +15,47 @@ $(document).ready(function() {
                 break;
         }
     });
-    $("#off").click(function() {
+    $("#off").click(function () {
         $("#forceStretch").prop("checked", false);
         $("#forceAspect").prop("checked", false);
-        chrome.storage.local.set({"extensionMode":0},function (){
+        chrome.storage.local.set({ "extensionMode": 0 }, function () {
         });
     });
 
-    $("#forceStretch").click(function() {
+    $("#forceStretch").click(function () {
         $("#off").prop("checked", false);
         $("#forceAspect").prop("checked", false);
-        chrome.storage.local.set({"extensionMode":1},function (){
+        chrome.storage.local.set({ "extensionMode": 1 }, function () {
         });
     });
-    $("#forceAspect").click(function() {
+    $("#forceAspect").click(function () {
         $("#off").prop("checked", false);
         $("#forceStretch").prop("checked", false);
-        chrome.storage.local.set({"extensionMode":2},function (){
+        chrome.storage.local.set({ "extensionMode": 2 }, function () {
         });
     });
-	
 
-
-	
-	
 });
 
+function getBase64FromImageUrl(url) {
+    var img = new Image();
+    img.setAttribute('crossOrigin', 'anonymous');
+    img.onload = function () {
+        var canvas = document.createElement("canvas");
+        canvas.width = this.width;
+        canvas.height = this.height;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(this, 0, 0);
+        // var dataURL = canvas.toDataURL("image/png");
+        var dataURL = canvas.toDataURL();
+        // alert(dataURL.replace(/^data:image\/(png|jpg);base64,/, ""));
+        // alert(dataURL);
+        return dataURL;
+    };
+    img.src = url;
+}
 
+function hexToBase64(str) {
+    return btoa(String.fromCharCode.apply(null, str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" ")));
+}
 
