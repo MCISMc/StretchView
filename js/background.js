15fa2ef9b8f874110a0609427e00b62ab8c04b7c
Pishang Ujeniya <pishangujeniya@outlook.com>
2019-01-08 18:32:42 +0530

    Dynamic Loading Images
    Dynamically Loading Images from Cache.
    Performance Improvement
    Loader added

--- js/background.js
+++ js/background.js
@@ -1,14 +1,14 @@
 chrome.runtime.onInstalled.addListener(function (details) {
     var manifest = chrome.runtime.getManifest();
     var version = (manifest.version);
-    if(details.reason == "update") {
+    if (details.reason == "update") {
         chrome.notifications.create(null, {
             type: "basic",
             iconUrl: "images/icon.png",
             title: "StretchView Add-On updated",
-            message: "Update log: updated to version "+version+" ",
-        }, function(updateNotificationId) {
-            chrome.notifications.onClicked.addListener(function(notificationId) {
+            message: "Update log: updated to version " + version + " ",
+        }, function (updateNotificationId) {
+            chrome.notifications.onClicked.addListener(function (notificationId) {
                 if (notificationId === updateNotificationId) {
                     var newURL = "https://github.com/MCISMc/StretchView";
                     chrome.tabs.create({ url: newURL });
@@ -17,6 +17,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
             });
         });
     }
-    chrome.storage.local.set({"extensionMode":0},function (){
-    }); 
+    chrome.storage.local.set({ "extensionMode": 0 }, function () {
+    });
+    localStorage.setItem('NumberOfImagesCached', 0);
 });
